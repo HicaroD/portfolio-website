fa7dc7bca29f8ee8b00f22762b266b894bb6f341
HicaroD <hdanrlley1@gmail.com>
2022-01-06 01:46:59 -0300

    Adding views to render templates for each section

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from .models import Article
 
 def index(request):
     return render(request, "portfolio/base.html")
+
+def home(request):
+    articles = Article.objects.all()
+    return render(request, "portfolio/home.html", {'articles':articles})
+
+def about_me(request):
+    return render(request, "portfolio/about_me.html")
+
+def contact(request):
+    return render(request, "portfolio/contact.html")
