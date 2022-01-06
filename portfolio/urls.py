4018d68019d184b5c5949c877c28732ccf716ce6
HicaroD <hdanrlley1@gmail.com>
2022-01-06 17:53:01 -0300

    Deleting 'home/' path because I'm mapping to '/'

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
         path('', views.home, name = "index"),
-        path('home/', views.home, name = "home"),
         path('about_me/', views.about_me, name = "about_me"),
         path('contact/', views.contact, name = "contact"),
 ]
