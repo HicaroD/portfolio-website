c0d47f9b0c13f30848fefa532da2d8aeecdd743d
HicaroD <hdanrlley1@gmail.com>
2022-01-06 01:48:13 -0300

    Mapping major sections of the page to a view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name = "index")
+        path('', views.home, name = "index"),
+        path('home/', views.home, name = "home"),
+        path('about_me/', views.about_me, name = "about_me"),
+        path('contact/', views.contact, name = "contact"),
 ]
