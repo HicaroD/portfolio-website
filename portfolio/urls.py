3540f4b37dcfeec85db46faf6c687ad0010c95e1
HicaroD <hdanrlley1@gmail.com>
2022-01-07 17:19:19 -0300

    Adding article/ to URL mapper variable

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
         path('', views.home, name = "index"),
         path('about_me/', views.about_me, name = "about_me"),
         path('contact/', views.contact, name = "contact"),
+        path('article/<int:article_id>', views.article, name = "article")
 ]
