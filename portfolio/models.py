ec60d8fb860277630110663c8a9f0a1ace3d521e
HicaroD <hdanrlley1@gmail.com>
2022-01-07 17:17:45 -0300

    Adding get_absolute_path for linking articles

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Article(models.Model):
     title = models.CharField(max_length=50)
@@ -8,6 +9,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("article", kwargs={'pk': self.pk})
 
     class Meta():
         ordering = ['-posted']
