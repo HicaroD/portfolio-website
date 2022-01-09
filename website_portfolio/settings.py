38f4f6f9882a964a928dd6258e9388ad08fbc201
HicaroD <hdanrlley1@gmail.com>
2022-01-09 16:39:27 -0300

    Adding Markdownify tags to translate

--- website_portfolio/settings.py
+++ website_portfolio/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio.apps.PortfolioConfig'
+    'portfolio.apps.PortfolioConfig',
+    'markdownify.apps.MarkdownifyConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,11 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-        (BASE_DIR / "portfolio/static"),
-]
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'portfolio/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MARKDOWNIFY = {
+  "default": {
+     "WHITELIST_TAGS": ["a", "p", "h1", "br", "ul", "li", "h1"
+                        "h2", "h3", "img"]
+  },
+}
