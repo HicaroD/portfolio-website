9ef15a0077108ce39bee00c05a6b57899c0a55bf
HicaroD <hdanrlley1@gmail.com>
2022-01-12 15:28:36 -0300

    Adding slug field to Article model

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,12 +6,13 @@ class Article(models.Model):
     posted = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     body = models.TextField()
+    slug = models.SlugField(max_length=50, null=True, unique=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("article", kwargs={'pk': self.pk})
+        return reverse("article", kwargs={'slug': self.slug})
 
     class Meta():
         ordering = ['-posted']
