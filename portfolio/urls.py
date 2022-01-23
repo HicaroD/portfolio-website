36fef5b6c50d33c8ca7183e79eb0ed4f769bd685
HicaroD <hdanrlley1@gmail.com>
2022-01-23 17:32:13 -0300

    Deleting article section

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.home, name = "index"),
-        path('about_me/', views.about_me, name = "about_me"),
+        path('', views.about_me, name = "about_me"),
         path('contact/', views.contact, name = "contact"),
         path('article/<slug:slug>', views.article, name = "article")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,10 +4,6 @@ from .models import Article
 def index(request):
     return render(request, "portfolio/base.html")
 
-def home(request):
-    articles = Article.objects.all()
-    return render(request, "portfolio/home.html", {'articles':articles})
-
 def about_me(request):
     return render(request, "portfolio/about_me.html")
 
