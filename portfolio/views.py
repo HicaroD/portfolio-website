a4764b3888fb487daa4856caac4dabf0ecadbf9a
HicaroD <hdanrlley1@gmail.com>
2022-02-10 18:11:59 -0300

    Creating Project view for rendering the template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
-from .models import Article
+from .models import Project
 
 def index(request):
     return render(request, "portfolio/base.html")
@@ -9,3 +9,6 @@ def about_me(request):
 
 def contact(request):
     return render(request, "portfolio/contact.html")
+
+def projects(request):
+    return render(request, "portfolio/projects.html")
