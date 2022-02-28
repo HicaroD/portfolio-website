61867643002bf015d29b98d7f8fd54b61b48dd34
HicaroD <hdanrlley1@gmail.com>
2022-02-28 17:46:23 -0300

    Adding List and detail view for Articles

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.detail import DetailView 
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "articles/article_list.html"
+    context_object_name  = "articles"
+
+class ArticleDetailView(DetailView):
+    model = Article
