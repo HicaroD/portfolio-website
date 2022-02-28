c56621c3eb4aada147b4be5ed72b0103b6a222dd
HicaroD <hdanrlley1@gmail.com>
2022-02-28 16:35:54 -0300

    Adding `articles/` url to main app

--- website_portfolio/urls.py
+++ website_portfolio/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    path('article/', include('articles.urls')),
 ]
