5997cac2b077c873aac7efa206188480a436ebf2
HicaroD <hdanrlley1@gmail.com>
2022-02-28 16:34:25 -0300

    Deleting useless comments

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,9 +4,9 @@ from django.urls import reverse
 class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
-    technologies = models.TextField(null=True)   # List of technologies separated by comma
+    technologies = models.TextField(null=True)
     project_link = models.CharField(max_length=100, null=True)
-    rating = models.SmallIntegerField() # For ordering
+    rating = models.SmallIntegerField()
 
     def __str__(self):
         return self.title
