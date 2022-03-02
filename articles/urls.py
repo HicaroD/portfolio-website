ca0724d3256cbd3b42e237f87af8816116444d88
HicaroD <hdanrlley1@gmail.com>
2022-03-02 17:08:26 -0300

    Removing `Article` app (not implementing)

--- articles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.ArticleListView.as_view(), name="article-list"),
-    path('<int:pk>/', views.ArticleDetailView.as_view(), name="article-detail"),
-]
