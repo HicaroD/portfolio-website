7702164676f1a5a7bf36b2467c705555c3036744
HicaroD <hdanrlley1@gmail.com>
2022-03-02 17:09:54 -0300

    Removing `articles/` on urlpatterns

--- website_portfolio/urls.py
+++ website_portfolio/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
-    path('article/', include('articles.urls')),
 ]
