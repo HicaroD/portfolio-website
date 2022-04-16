86474c5d7ea44feb788a6f0bc671a0b4685ebe52
HicaroD <hdanrlley1@gmail.com>
2022-04-16 01:10:08 -0300

    [Backend] Changing view name to `articles`

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ArticleListView.as_view(), name="homepage"),
+    path('', views.ArticleListView.as_view(), name="articles"),
     path('<slug:slug>', views.ArticleDetailView.as_view(), name="article_detail")
 ]
